eae9a093dc2c160e3a267c30b85c19db6720e1c2
Nikita Polovinkin <poloniki@ya.ru>
2023-10-13 19:51:36 +0200

    Original version

--- pipeline.py
+++ pipeline.py
@@ -1,23 +1,24 @@
+# sudo pip3 install kfp --pre
 import kfp
 from kfp import compiler
 
 import kfp.dsl as dsl
 
+
 @dsl.container_component
 def whisper_jax():
     return dsl.ContainerSpec(
-        image='us-central1-docker.pkg.dev/PROJECT/REPOSITORY/IMAGE:TAG',
+        image="europe-southwest1-docker.pkg.dev/wagon-bootcamp-355610/whisper-jax/pipeline-task:latest",
         args=[
             "--audio_file",
-            "gs://BUCKET/example.flac",
+            "gs://taxifare_super_bucket/myfile.mp3",
             "--transcript_file",
-            "gs://BUCKET/example.txt",
-        ]
+            "gs://taxifare_super_bucket/example.txt",
+        ],
     )
 
-@dsl.pipeline(
-    name="whisper-jax"
-)
+
+@dsl.pipeline(name="whisper-jax")
 def transcribe_pipeline():
     task = whisper_jax()
     task.set_cpu_request("4")
@@ -27,4 +28,5 @@ def transcribe_pipeline():
     task.set_accelerator_type("NVIDIA_TESLA_T4")
     task.set_accelerator_limit(1)
 
-compiler.Compiler().compile(transcribe_pipeline, 'pipeline.json')
+
+compiler.Compiler().compile(transcribe_pipeline, "pipeline.json")
